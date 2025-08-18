3e927c3f6b7350817f2e16b8d8aebaa3023f32cc
acandoo <117209328+acandoo@users.noreply.github.com>
2025-08-18 02:55:30 -0400

    Add smooth scrolling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  site: 'https://aeondesktop.github.io',
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Source Sans 3',
+        cssVariable: '--font-source-sans',
+        weights: [400, 700]
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Inter',
+        cssVariable: '--font-inter',
+        weights: [400, 700]
+      }
+    ]
+  }
+})
